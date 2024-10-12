962f6ee1c19b04282da679529faba30e93695cc6
Yipeng Sun <syp@umd.edu>
2024-10-13 01:56:29 +0800

    infra: try building on Windows with my dependency installer.

---
+++ tools/install-deps.ps1
@@ -0,0 +1,39 @@
+function WinGet-Install {
+param(
+    [string]$Package
+)
+    Write-Output "install $Package..."
+    winget install -e --id $Package -s winget `
+        --no-upgrade `
+        --silent `
+        --accept-package-agreements `
+        --accept-source-agreements
+}
+
+function WinGet-Install-All {
+param(
+    [string[]]$Packages
+)
+    foreach ($pkg in $Packages) {
+        WinGet-Install $pkg
+    }
+}
+
+
+$pkgs = @(
+    "Kitware.CMake"
+    "DimitriVanHeesch.Doxygen"
+    "Graphviz.Graphviz"
+)
+
+WinGet-Install-All $pkgs
+
+
+# msvc
+winget install -e --id Microsoft.VisualStudio.2022.BuildTools `
+    -s winget `
+    --no-upgrade `
+    --silent `
+    --accept-package-agreements `
+    --accept-source-agreements `
+    --override '--passive --wait --add Microsoft.VisualStudio.Workload.VCTools;includeRecommended'
